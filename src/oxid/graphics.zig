5a1d29e88eb3b2b8d73769e5054027f7007bbb1e
dbandstra <dbandstra@protonmail.com>
2018-07-28 20:19:16 -0700

    graphics-related rearrangement
    stuff

--- src/oxid/graphics.zig
+++ src/oxid/graphics.zig
@@ -1,8 +1,7 @@
+const std = @import("std");
 const DoubleStackAllocatorFlat = @import("../../zigutils/src/DoubleStackAllocatorFlat.zig").DoubleStackAllocatorFlat;
-const MemoryInStream = @import("../../zigutils/src/MemoryInStream.zig").MemoryInStream;
 const image = @import("../../zigutils/src/image/image.zig");
-const LoadPcx = @import("../../zigutils/src/image/pcx.zig").LoadPcx;
-const pcxBestStoreFormat = @import("../../zigutils/src/image/pcx.zig").pcxBestStoreFormat;
+const pcx = @import("../../zig-comptime-pcx/pcx.zig");
 
 const Platform = @import("../platform/platform.zig");
 
@@ -21,43 +20,30 @@ pub const Graphics = struct{
 };
 
 // return an Image allocated in the low_allocator
-fn loadTileset(dsaf: *DoubleStackAllocatorFlat, out_background_colour: *image.Pixel) !*image.Image {
-  const high_mark = dsaf.get_high_mark();
-  defer dsaf.free_to_high_mark(high_mark);
+fn loadTileset(dsaf: *DoubleStackAllocatorFlat) !*image.Image {
+  const filedata = @embedFile(GRAPHICS_FILENAME);
 
-  var source = MemoryInStream.init(@embedFile(GRAPHICS_FILENAME));
+  var slice_stream = std.io.SliceInStream.init(filedata);
+  var stream = &slice_stream.stream;
+  const Loader = pcx.Loader(std.io.SliceInStream.Error);
 
   // load pcx
-  const pcxInfo = try LoadPcx(MemoryInStream.ReadError).preload(&source.stream, &source.seekable);
-  const img = try image.createImage(&dsaf.high_allocator, image.Info{
-    .width = pcxInfo.width,
-    .height = pcxInfo.height,
-    .format = pcxBestStoreFormat(pcxInfo),
-  });
-  try LoadPcx(MemoryInStream.ReadError).load(&source.stream, &source.seekable, pcxInfo, img);
-
-  // load palette
-  const palette = try image.createPalette(&dsaf.high_allocator);
-  try LoadPcx(MemoryInStream.ReadError).loadPalette(&source.stream, &source.seekable, pcxInfo, palette);
-
-  // convert to true color image
-  const img2 = try image.createImage(&dsaf.low_allocator, image.Info{
-    .width = img.info.width,
-    .height = img.info.height,
+  const preloaded = try Loader.preload(stream);
+  const img = try image.createImage(&dsaf.low_allocator, image.Info{
+    .width = preloaded.width,
+    .height = preloaded.height,
     .format = image.Format.RGBA,
   });
-  image.convertToTrueColor(img2, img, palette, TRANSPARENT_COLOR_INDEX);
+  try Loader.loadRGBA(stream, &preloaded, TRANSPARENT_COLOR_INDEX, img.pixels);
 
-  out_background_colour.* = image.getColor(palette.format, palette.data, TRANSPARENT_COLOR_INDEX);
-
-  return img2;
+  return img;
 }
 
 pub fn loadGraphics(dsaf: *DoubleStackAllocatorFlat, graphics: *Graphics) !void {
   const low_mark = dsaf.get_low_mark();
   defer dsaf.free_to_low_mark(low_mark);
 
-  const tileset = try loadTileset(dsaf, &graphics.background_colour);
+  const tileset = try loadTileset(dsaf);
 
   const tile = try image.createImage(&dsaf.low_allocator, image.Info{
     .width = 16,
@@ -83,8 +69,11 @@ fn extractTile(dest: *image.Image, source: *const image.Image, tx: u32, ty: u32)
     while (x < dest.info.width) : (x += 1) {
       const px = tx * dest.info.width + x;
       const py = ty * dest.info.height + y;
-      const pixel = image.getPixel(source, px, py).?;
-      image.setPixel(dest, x, y, pixel);
+      if (image.getPixel(source, px, py)) |pixel| {
+        image.setPixel(dest, x, y, pixel);
+      } else {
+        image.setPixel(dest, x, y, image.Pixel{ .r = 0, .g = 0, .b = 0, .a = 0 });
+      }
     }
   }
 }
