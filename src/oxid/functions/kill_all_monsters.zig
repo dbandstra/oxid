8eb570534056a7f431705f142ac6d2cf0106bc34
dbandstra <dbandstra@protonmail.com>
2018-08-20 22:37:13 -0700

    skip wave cheat should work even before 1st wave

--- src/oxid/functions/kill_all_monsters.zig
+++ src/oxid/functions/kill_all_monsters.zig
@@ -6,19 +6,14 @@ const C = @import("../components.zig");
 
 // this is a cheat
 pub fn killAllMonsters(gs: *GameSession) void {
-  var killed_any = false;
-
   var it = gs.gbe.iter(C.Monster); while (it.next()) |object| {
     if (!object.data.persistent) {
       gs.gbe.markEntityForRemoval(object.entity_id);
-      killed_any = true;
     }
   }
   gs.gbe.applyRemovals();
 
-  if (killed_any) {
-    if (gs.gbe.iter(C.GameController).next()) |object| {
-      object.data.next_wave_timer = 1;
-    }
+  if (gs.gbe.iter(C.GameController).next()) |object| {
+    object.data.next_wave_timer = 1;
   }
 }
