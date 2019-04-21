5eb4ebb2077e08150b1d3a50d019a7b2af7abdec
dbandstra <dbandstra@protonmail.com>
2019-04-21 16:42:09 -0700

    update zang library

--- src/audio/explosion.zig
+++ src/audio/explosion.zig
@@ -3,52 +3,47 @@ const zang = @import("zang");
 pub const ExplosionVoice = struct {
   pub const NumTempBufs = 3;
 
-  cutoff_tracker: zang.CurveTracker,
-  volume_tracker: zang.CurveTracker,
-
-  curve: zang.Curve,
+  cutoff_curve: zang.Curve,
+  volume_curve: zang.Curve,
   noise: zang.Noise,
   filter: zang.Filter,
 
   pub fn init() ExplosionVoice {
     return ExplosionVoice {
-      .cutoff_tracker = zang.CurveTracker.init([]zang.CurveTrackerNode {
-        zang.CurveTrackerNode{ .t = 0.0, .value = 3000.0 },
-        zang.CurveTrackerNode{ .t = 0.5, .value = 1000.0 },
-        zang.CurveTrackerNode{ .t = 0.7, .value = 200.0 },
+      .cutoff_curve = zang.Curve.init(.SmoothStep, []zang.CurveNode {
+        zang.CurveNode{ .t = 0.0, .value = 3000.0 },
+        zang.CurveNode{ .t = 0.5, .value = 1000.0 },
+        zang.CurveNode{ .t = 0.7, .value = 200.0 },
       }),
-      .volume_tracker = zang.CurveTracker.init([]zang.CurveTrackerNode {
-        zang.CurveTrackerNode{ .t = 0.0, .value = 0.0 },
-        zang.CurveTrackerNode{ .t = 0.004, .value = 0.75 },
-        zang.CurveTrackerNode{ .t = 0.7, .value = 0.0 },
+      .volume_curve = zang.Curve.init(.SmoothStep, []zang.CurveNode {
+        zang.CurveNode{ .t = 0.0, .value = 0.0 },
+        zang.CurveNode{ .t = 0.004, .value = 0.75 },
+        zang.CurveNode{ .t = 0.7, .value = 0.0 },
       }),
-      .curve = zang.Curve.init(.SmoothStep),
       .noise = zang.Noise.init(0),
       .filter = zang.Filter.init(.LowPass, 0.0, 0.0),
     };
   }
 
   pub fn paint(self: *ExplosionVoice, sample_rate: f32, out: []f32, note_on: bool, freq: f32, tmp: [3][]f32) void {
-    const cutoff_curve = self.cutoff_tracker.getCurveNodes(sample_rate, out.len);
-    const volume_curve = self.volume_tracker.getCurveNodes(sample_rate, out.len);
-
-    for (cutoff_curve) |*node| {
-      node.value = zang.cutoffFromFrequency(node.value, sample_rate);
-    }
-
     zang.zero(tmp[0]);
     self.noise.paint(tmp[0]);
     zang.zero(tmp[1]);
-    self.curve.paintFromCurve(tmp[1], cutoff_curve, freq);
+    self.cutoff_curve.paint(sample_rate, tmp[1], freq);
+    // FIXME - apply this to the curve nodes before interpolation, to save
+    // time. but this probably requires a change to the zang api
+    var i: usize = 0; while (i < tmp[1].len) : (i += 1) {
+      tmp[1][i] = zang.cutoffFromFrequency(tmp[1][i], sample_rate);
+    }
     zang.zero(tmp[2]);
     self.filter.paintControlledCutoff(sample_rate, tmp[2], tmp[0], tmp[1]);
     zang.zero(tmp[1]);
-    self.curve.paintFromCurve(tmp[1], volume_curve, null);
+    self.volume_curve.paint(sample_rate, tmp[1], null);
     zang.multiply(out, tmp[2], tmp[1]);
   }
 
   pub fn reset(self: *ExplosionVoice) void {
-    self.cutoff_tracker.reset();
-    self.volume_tracker.reset();
+    self.cutoff_curve.reset();
+    self.volume_curve.reset();
   }
 };
--- src/audio/laser.zig
+++ src/audio/laser.zig
@@ -3,70 +3,60 @@ const zang = @import("zang");
 pub const LaserVoice = struct {
   pub const NumTempBufs = 3;
 
-  carrier_tracker: zang.CurveTracker,
-  modulator_tracker: zang.CurveTracker,
-  volume_tracker: zang.CurveTracker,
-
   carrier_mul: f32,
+  carrier_curve: zang.Curve,
+  carrier: zang.Oscillator,
   modulator_mul: f32,
   modulator_rad: f32,
-  curve: zang.Curve,
-  carrier: zang.Oscillator,
+  modulator_curve: zang.Curve,
   modulator: zang.Oscillator,
+  volume_curve: zang.Curve,
 
   pub fn init(carrier_mul: f32, modulator_mul: f32, modulator_rad: f32) LaserVoice {
-    const A = 1000.0;
-    const B = 200.0;
-    const C = 100.0;
-
     return LaserVoice {
-      .carrier_tracker = zang.CurveTracker.init([]zang.CurveTrackerNode {
-        zang.CurveTrackerNode{ .value = A, .t = 0.0 },
-        zang.CurveTrackerNode{ .value = B, .t = 0.1 },
-        zang.CurveTrackerNode{ .value = C, .t = 0.2 },
-      }),
-      .modulator_tracker = zang.CurveTracker.init([]zang.CurveTrackerNode {
-        zang.CurveTrackerNode{ .value = A, .t = 0.0 },
-        zang.CurveTrackerNode{ .value = B, .t = 0.1 },
-        zang.CurveTrackerNode{ .value = C, .t = 0.2 },
-      }),
-      .volume_tracker = zang.CurveTracker.init([]zang.CurveTrackerNode {
-        zang.CurveTrackerNode{ .value = 0.0, .t = 0.0 },
-        zang.CurveTrackerNode{ .value = 0.35, .t = 0.004 },
-        zang.CurveTrackerNode{ .value = 0.0, .t = 0.2 },
-      }),
       .carrier_mul = carrier_mul,
+      .carrier_curve = zang.Curve.init(.SmoothStep, []zang.CurveNode {
+        zang.CurveNode{ .value = 1000.0, .t = 0.0 },
+        zang.CurveNode{ .value = 200.0, .t = 0.1 },
+        zang.CurveNode{ .value = 100.0, .t = 0.2 },
+      }),
+      .carrier = zang.Oscillator.init(.Sine),
       .modulator_mul = modulator_mul,
       .modulator_rad = modulator_rad,
-      .curve = zang.Curve.init(.SmoothStep),
-      .carrier = zang.Oscillator.init(.Sine),
+      .modulator_curve = zang.Curve.init(.SmoothStep, []zang.CurveNode {
+        zang.CurveNode{ .value = 1000.0, .t = 0.0 },
+        zang.CurveNode{ .value = 200.0, .t = 0.1 },
+        zang.CurveNode{ .value = 100.0, .t = 0.2 },
+      }),
       .modulator = zang.Oscillator.init(.Sine),
+      .volume_curve = zang.Curve.init(.SmoothStep, []zang.CurveNode {
+        zang.CurveNode{ .value = 0.0, .t = 0.0 },
+        zang.CurveNode{ .value = 0.35, .t = 0.004 },
+        zang.CurveNode{ .value = 0.0, .t = 0.2 },
+      }),
     };
   }
 
   pub fn paint(self: *LaserVoice, sample_rate: f32, out: []f32, note_on: bool, freq: f32, tmp: [3][]f32) void {
-    const carrier_curve = self.carrier_tracker.getCurveNodes(sample_rate, out.len);
-    const modulator_curve = self.modulator_tracker.getCurveNodes(sample_rate, out.len);
-    const volume_curve = self.volume_tracker.getCurveNodes(sample_rate, out.len);
     const freq_mul = freq;
 
     zang.zero(tmp[0]);
-    self.curve.paintFromCurve(tmp[0], modulator_curve, freq_mul * self.modulator_mul);
+    self.modulator_curve.paint(sample_rate, tmp[0], freq_mul * self.modulator_mul);
     zang.zero(tmp[1]);
     self.modulator.paintControlledFrequency(sample_rate, tmp[1], tmp[0]);
     zang.multiplyWithScalar(tmp[1], self.modulator_rad);
     zang.zero(tmp[0]);
-    self.curve.paintFromCurve(tmp[0], carrier_curve, freq_mul * self.carrier_mul);
+    self.carrier_curve.paint(sample_rate, tmp[0], freq_mul * self.carrier_mul);
     zang.zero(tmp[2]);
     self.carrier.paintControlledPhaseAndFrequency(sample_rate, tmp[2], tmp[1], tmp[0]);
     zang.zero(tmp[0]);
-    self.curve.paintFromCurve(tmp[0], volume_curve, null);
+    self.volume_curve.paint(sample_rate, tmp[0], null);
     zang.multiply(out, tmp[0], tmp[2]);
   }
 
   pub fn reset(self: *LaserVoice) void {
-    self.carrier_tracker.reset();
-    self.modulator_tracker.reset();
-    self.volume_tracker.reset();
+    self.carrier_curve.reset();
+    self.modulator_curve.reset();
+    self.volume_curve.reset();
   }
 };
